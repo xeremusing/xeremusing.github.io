20bd79a5e8a1a9fc1ca3e3f687c3910955249e08
xeremusing <puckmyduck@gmail.com>
2024-11-26 19:01:53 -0600

    Add files via upload
    the one and only assignment 5

---
+++ assignment5-folder/app.js
@@ -0,0 +1,14 @@
+async function getData(selectedMajor) {
+		var response = await fetch('cit5students.json');
+		if(response.ok {
+			var data = await response.json();
+			stuMajor = data.filter( (item) => student.major == selectedMajor );
+			var templateText = document.getElementById('classTemplate').innerHTML;
+			var compiledTemplateText = Handlebars.compile(templateText);
+			compiledHtml = compiledTemplateText({ rows: stuMajor })
+			document.getElementById('classTable').innerHTML = compiledHtml;
+		}
+		else {
+			document.querySelector('#classTable').innerHTML = "There was either an error or a student's data could not be found";
+		}
+}
